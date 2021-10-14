9e5488f68421175de9245d00b6bbd4d8eafcdc73
Izhar-Zour <90437474+Izhar-Nabi@users.noreply.github.com>
2021-10-14 18:35:26 +0500

    Even And Odd Numbers using Array

---
+++ EvenAndOddInArray.java
@@ -0,0 +1,12 @@
+class EvenAndOddInArray
+{
+	public static void main(String args[]){
+	int num[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	for(int i=0; i<10; i++){
+	if(num[i]%2==0){
+	System.out.println("It is an Even Number "+ num[i]);}
+	else if(num[i]%2 !=0) {
+	System.out.println("It is an Odd Number "+num[i]);}
+		}
+	}
+}
